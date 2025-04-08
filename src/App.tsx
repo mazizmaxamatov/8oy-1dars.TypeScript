288cad81d9e6b0db607885fa07cdd49978d1e037
muxammadazizjon <mazizmaxamatov@gmail.com>
2025-04-08 14:52:02 +0500

    salom

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import './App.css'
+
+function App() {
+
+
+  let name: string = "Muxammadazizjon";
+  console.log("Name:", name);
+
+
+  let age: number = 19;
+  let newAge: number = age + 20;
+  console.log("New Age:", newAge);
+
+
+  let bir: boolean = true;
+  console.log("Siz dasturchimsiz?", bir);
+
+
+  let meva: string[] = ["Olma", "Banan", "Kivi"];
+  console.log("mevalar:", meva);
+
+
+  let ikki: { name: string, age: number, isStudent: boolean } = {
+    name: "Muxammadazizjon",
+    age: 20,
+    isStudent: false
+  };
+  console.log("Person Object:", ikki);
+
+
+
+  return (
+    <>
+
+      <p>Name: {name}</p>
+      <p>Yosh: {newAge}</p>
+      <p>Siz dasturchimsiz? {bir}</p>
+      <p>Yaxshi korgan mevalarim: {meva}</p>
+      
+    </>
+  )
+}
+
+export default App
